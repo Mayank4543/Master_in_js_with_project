9c91ef7280fecc516e250632a75c7f302a5bc6ed
Mayank4543 <134360258+Mayank4543@users.noreply.github.com>
2023-11-20 22:57:08 +0530

    function

---
+++ function/index.js
@@ -0,0 +1,38 @@
+// function saynme() {
+//   console.log("hi");
+//   console.log("hello");
+// }
+// saynme();
+// function addTonumber(number1, number2) {
+//   return number1 + number2;
+// }
+// console.log(addTonumber(1, 2));
+// function gumnaam(username = "mayank") {
+//   if (username) {
+//     return `${username} is visible`;
+//   } else {
+//     return `${username} is not visible`;
+//   }
+// }
+// console.log(gumnaam());
+// function calcluateCartPice(...num1) {
+//   return num1;
+// }
+// console.log(calcluateCartPice(200, 450, 400, 500));
+// // ...num1-> is rest operator which bind the data in a single box we can say
+// const user = {
+//   username: "mayank",
+//   price: "300",
+// };
+// function bindii(anyobject) {
+//   console.log(
+//     `username is ${anyobject.username} and the price is ${anyobject.price}`
+//   );
+// }
+
+// bindii(user);
+const myarray = [2, 24, 5, 6, 7, 8];
+function arr(getaraya) {
+  return getaraya[4];
+}
+console.log(arr(myarray));
