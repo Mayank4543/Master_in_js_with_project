97a54bd822d1b619f4d2206d89b106972bc9cb5e
Maynak4543 <mayankrathore9897@gmail.com>
2024-05-16 22:50:35 +0530

    sum of number in question js

--- interview/question.js
+++ interview/question.js
@@ -1,7 +1,16 @@
-const arr = ["babyies", "shy", "is", "cute"];
-console.log(arr[0].length);
-if (arr[0].length == 4) {
-  console.log("babyiscute");
-} else {
-  console.log("baby is not cute");
+// const arr = ["baby", "shy", "is", "cute"];
+// console.log(arr[0].length);
+// if (arr[0].length == 4) {
+//   console.log("babyiscute");
+// } else {
+//   console.log("baby is not cute");
+// }
+
+function sumofelemnt(num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum = sum + i;
+  }
+  console.log(sum);
 }
+sumofelemnt(6);
