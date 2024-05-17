77aa53557eb8d807134926a1809e1384db9093b8
Maynak4543 <mayankrathore9897@gmail.com>
2024-05-17 23:52:06 +0530

    split the string

---
+++ interview/mr.js
@@ -0,0 +1,11 @@
+const str = "ANnurag Sharma";
+let str2 = " ";
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] >= "A" && str[i] <= "Z") {
+//     str2 += str[i];
+//   }
+// }
+
+// const strr = str.split(" ").map((item) => item.charAt(0));
+
+console.log(str2);
