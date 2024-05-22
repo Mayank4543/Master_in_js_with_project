7007be2a331db54a9391b54da47be90eb32710a0
Maynak4543 <mayankrathore9897@gmail.com>
2024-05-22 23:40:02 +0530

    fibonacci sequence

--- interview/question.js
+++ interview/question.js
@@ -36,3 +36,28 @@ function findLargestElement(nestedArray) {
 
   return largest;
 }
+
+function fibonacciSequence(numTerms) {
+  // if numTerms is less than or equal to 0, return an empty array
+  if (numTerms <= 0) return [];
+
+  // if numTerms is 1, return an array with a single element 0
+  if (numTerms === 1) return [0];
+
+  // initialize the sequence array with the first two Fibonacci numbers
+  let sequence = [0, 1];
+  console.log(sequence.length);
+  // loop until the sequence has the desired number of terms
+  while (sequence.length < numTerms) {
+    // calculate the next Fibonacci number by adding the last two numbers in the sequence
+    let nextNumber =
+      sequence[sequence.length - 1] + sequence[sequence.length - 2];
+
+    // add the next number to the sequence
+    sequence.push(nextNumber);
+  }
+
+  // return the completed Fibonacci sequence
+  return sequence;
+}
+console.log(fibonacciSequence(10));
