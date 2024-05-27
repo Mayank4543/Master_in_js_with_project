7b4381b8f45931756888d3b2bd4d02f442802e4d
Maynak4543 <mayankrathore9897@gmail.com>
2024-05-27 23:30:39 +0530

    romantointeger

---
+++ interview/romantointeger.js
@@ -0,0 +1,33 @@
+class Solution {
+  romanToInt(s) {
+    const roman = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+
+    let total = 0;
+    const n = s.length;
+
+    for (let i = 0; i < n; i++) {
+      // If the current numeral is less than the next numeral, subtract its value
+      if (i < n - 1 && roman[s[i]] < roman[s[i + 1]]) {
+        total -= roman[s[i]];
+      } else {
+        // Otherwise, add its value
+        total += roman[s[i]];
+      }
+    }
+
+    return total;
+  }
+}
+
+// Example usage:
+const solution = new Solution();
+console.log(solution.romanToInt("III")); // Output: 3
+console.log(solution.romanToInt("LVIII")); // Output:
