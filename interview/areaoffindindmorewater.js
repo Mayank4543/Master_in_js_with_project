3b256dd06f5f137db1d769206789515b8af89c6a
Maynak4543 <mayankrathore9897@gmail.com>
2024-05-29 23:53:48 +0530

    calculate maximum area of water in container contain

---
+++ interview/areaoffindindmorewater.js
@@ -0,0 +1,19 @@
+function maxim(height) {
+  var left = 0;
+  var right = height.length - 1;
+  var maxarea = 0;
+  while (left < right) {
+    var currheight = Math.min(height[left], height[right]);
+    var currentwidth = right - left;
+    var currentarea = currentwidth * currheight;
+    maxarea = Math.max(maxarea, currentarea);
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxarea;
+}
+var height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+console.log(maxim(height));
