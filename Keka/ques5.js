1958e7c6e3e1be0d2fe1abb1d8a40bd3862cf44c
Maynak4543 <mayankrathore9897@gmail.com>
2024-06-07 23:33:13 +0530

    factorial

--- Keka/ques5.js
+++ Keka/ques5.js
@@ -2,3 +2,13 @@ function evenfilternumbers(numbers) {
   return numbers.filter((num) => num % 2 == 0);
 }
 console.log(evenfilternumbers([24, 23, 22, 11, 10]));
+// quest6
+// factorial of a numbers
+function factorial(num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+console.log(factorial(5));
