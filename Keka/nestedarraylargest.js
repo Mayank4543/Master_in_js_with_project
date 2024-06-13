5ac80c00a9fab0301a0d2bfdae257c57ba389051
Maynak4543 <mayankrathore9897@gmail.com>
2024-06-13 21:54:54 +0530

    fibbonaci example

--- Keka/nestedarraylargest.js
+++ Keka/nestedarraylargest.js
@@ -16,22 +16,5 @@ let nestedArray = [
 ];
 
 console.log(findlargetsnested(nestedArray));
-// function fibonacciSequence(numTerms) {
-
-//   if (numTerms <= 0) return [];
-
-//   if (numTerms === 1) return [0];
-
-//   let sequence = [0, 1];
-
-//   while (sequence.length < numTerms) {
-
-//     let nextNumber = sequence[sequence.length – 1] + sequence[sequence.length – 2];
-
-//     sequence.push(nextNumber);
-
-//   }
-
-//   return sequence;
-
-// }
+let result = fibonacciSequence(7);
+console.log(result);
